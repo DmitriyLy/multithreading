154812d42e1e66031af719055303c8748d2423c2
Dmytro Lysai <dmly0217@netcracker.com>
2021-05-25 13:53:48 +0300

    2

--- src/main/java/org/dmly/multithreading/start/CurrentThread.java
+++ src/main/java/org/dmly/multithreading/start/CurrentThread.java
@@ -1,2 +1,21 @@
-package org.dmly.multithreading.start;public class CurrentThread {
+package org.dmly.multithreading.start;
+
+public class CurrentThread {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+
+        System.out.println("Current thread: " + thread);
+
+        thread.setName("This current thread");
+        System.out.println("New name set: " + thread);
+
+        try {
+            for (int n = 5; n > 0; n--) {
+                System.out.println(n);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
